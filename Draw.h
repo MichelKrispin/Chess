8eef828ddad40cd060ad6555866eb066e834ecbc
janlucian <jan.lucian.h3@gmail.com>
2019-01-07 18:53:24 +0100

    draw not finished

--- Draw.h
+++ Draw.h
@@ -1,9 +1,16 @@
-void Draw()
-{
-    COLOR_RESET;
-    setlocale(LC_ALL, "");
-    BG_WHITE;
-    printf("%lc", (wint_t) 9812);
-    BG_BLACK;
-    printf("%lc\n", (wint_t) 9812);
+void Draw(unsigned int field[8][8][2]){
+
+    for(unsigned int rowcount = 0; rowcount < 8; rowcount++){   // Zeilenschleife
+        for(unsigned int columnc = 0; columnc < 8; columnc++){  // Spaltenschleife
+            if((rowcount + columnc) % 2 == 0)                   // Hintergrund (s/w)
+                BG_WHITE;            
+            else
+                BG_BLACK;
+            switch(field[rowcount][columnc][0]){                // Figuren (Buchst.)
+                case 1: printf(" P ");
+                default: printf("   ");
+            }
+        }
+        printf("\n");                                           // Zeilenumbruch
+    }
 } 
--- Main.c
+++ Main.c
@@ -6,11 +6,29 @@
 
 int main()
 {
-
+    unsigned int field[8][8][2] = {0};                          // Array Initialisieren
+    for(unsigned int rowcount = 0; rowcount < 8; rowcount++){
+        for(unsigned int columnc = 0; columnc < 8; columnc++){
+            if((rowcount == 1) || (rowcount == 6))              // Bauern
+                field[rowcount][columnc][0] = 1;
+            else if(rowcount == 0){                             // Restfiguren
+                if(columnc < 4)
+                    field[rowcount][columnc][0] = columnc + 2;
+                else if()
+                else
+                    field[rowcount][columnc][0] = 9 - columnc;
+            }
+            if(rowcount > 5)
+                field[rowcount][columnc][1] = 1;                // weiße Farbe
+        }
+    }
+    BG_WHITE;
     COLOR_RED;
     printf("Hello World!\n");
     COLOR_YELLOW;
+    BG_BLACK;
     printf("Hello World!\n");
+    Draw(field);                            // Funktion
 
-   return 0;
+    return 0;
 }
