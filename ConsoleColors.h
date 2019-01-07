24ba0ae123dbf90001f00f89c49d871122e4b295
Michel <michel.krispin@gmail.com>
2019-01-07 17:06:39 +0100

    Added ConsoleColors.h

---
+++ ConsoleColors.h
@@ -0,0 +1,15 @@
+#ifndef CONSOLECOLORS_H
+#define CONSOLECOLORS_H
+
+
+// Colors which should be set in before each printf command
+#define COLOR_RESET  printf("\x1B[0m")
+#define COLOR_RED    printf("\x1B[31m")
+#define COLOR_GREEN  printf("\x1B[32m")
+#define COLOR_YELLOW printf("\x1B[33m")
+#define COLOR_BLUE   printf("\x1b[34m")
+#define COLOR_CYAN   printf("\x1B[36m")
+
+
+
+#endif // CONSOLECOLORS_H
--- Main.c
+++ Main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include "Draw.h"
+#include "ConsoleColors.h"
 
-int main(){
+int main()
+
+{
+    COLOR_RED;
+    printf("Hello World!\n");
+    COLOR_YELLOW;
     printf("Hello World!\n");
     return 0;
 }
