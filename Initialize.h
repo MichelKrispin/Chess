825245a1cf2b80e69a269b867ab1d5562cfd6cc8
Michel <michel.krispin@gmail.com>
2019-01-08 14:57:55 +0100

    Set array to be figures

--- Initialize.h
+++ Initialize.h
@@ -1,6 +1,6 @@
 void InitializeField(unsigned int field[][8][2])
 {
-    // Set all third array elements to be either 0 as empty
+    // Set all array elements to be either 0 as empty
     // Or numbers for different figure
     // Pawn  = 1
     // Tower = 2
@@ -8,18 +8,25 @@ void InitializeField(unsigned int field[][8][2])
     // Bishop = 4
     // Queen = 5
     // King = 6
+    // Third element indicates team color
+    // -> 0 = Black
+    // -> 1 = White
 
     for(unsigned int rowcount = 0; rowcount < 8; rowcount++)
     {
         for(unsigned int columnc = 0; columnc < 8; columnc++)
         {
-            if((rowcount == 1) || (rowcount == 6))              // Bauern
+            // Pawns
+            if((rowcount == 1) || (rowcount == 6))
                 field[rowcount][columnc][0] = 1;
-            else if(rowcount == 0)
-            {                             // Restfiguren
-                if(columnc < 4)
+
+            // All other figures -> Last and first row
+            else if(rowcount == 0 || rowcount == 7)
+            {
+                // Counting up the columns
+                if(columnc < 5)
                     field[rowcount][columnc][0] = columnc + 2;
-                //else if()
+
                 else
                     field[rowcount][columnc][0] = 9 - columnc;
             }
