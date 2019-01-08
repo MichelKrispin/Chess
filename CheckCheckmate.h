cb99fd77a0c4d6fe5504677e5cc7eb92fcb1b614
Michel <michel.krispin@gmail.com>
2019-01-08 16:33:13 +0100

    Silenced warnings

--- CheckCheckmate.h
+++ CheckCheckmate.h
@@ -1,5 +1,7 @@
 // Check whether any King is now in Checkmate
 int CheckCheckmate(unsigned int field[][8][2])
 {
-
+    int a = field[0][0][0];
+    a = 1;
+    return a;
 }
--- CheckLogic.h
+++ CheckLogic.h
@@ -4,5 +4,10 @@ int CheckLogic(unsigned int startrow, unsigned int startcolumn,
                unsigned int destrow, unsigned int destcolumn,
                unsigned int field[][8][2])
 {
+    startrow = 1;
+    startcolumn = 1;
+    destrow = 1;
+    destcolumn = field[0][0][0];
 
+    return 1;
 }
