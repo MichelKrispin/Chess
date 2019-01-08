902a4062bb252a3891cf9a35136043b8e1138602
Michel <michel.krispin@gmail.com>
2019-01-08 13:51:46 +0100

    Used memset to set all elements 0

--- Main.c
+++ Main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <wchar.h>
 #include <locale.h>
+#include <string.h>
 #include "ConsoleColors.h"
 #include "Draw.h"
 
 int main()
 {
-    unsigned int field[8][8][2] = {0};                          // Array Initialisieren
+    unsigned int field[8][8][2];                          // Array Initialisieren
+    memset(field, 0, sizeof(field)); // Set all elements to 0
+
     for(unsigned int rowcount = 0; rowcount < 8; rowcount++){
         for(unsigned int columnc = 0; columnc < 8; columnc++){
             if((rowcount == 1) || (rowcount == 6))              // Bauern
@@ -14,7 +17,7 @@ int main()
             else if(rowcount == 0){                             // Restfiguren
                 if(columnc < 4)
                     field[rowcount][columnc][0] = columnc + 2;
-                else if()
+                //else if()
                 else
                     field[rowcount][columnc][0] = 9 - columnc;
             }
@@ -22,12 +25,7 @@ int main()
                 field[rowcount][columnc][1] = 1;                // weiße Farbe
         }
     }
-    BG_WHITE;
-    COLOR_RED;
-    printf("Hello World!\n");
-    COLOR_YELLOW;
-    BG_BLACK;
-    printf("Hello World!\n");
+    
     Draw(field);                            // Funktion
 
     return 0;
