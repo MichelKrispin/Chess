a89cdad671fbfac24d807ec91cfffea5c57cdc16
janlucian <jan.lucian.h3@gmail.com>
2019-01-08 15:50:30 +0100

    move added

--- Move.h
+++ Move.h
@@ -1,4 +1,6 @@
 int Move(unsigned int row, unsigned int column, unsigned int field[][8][2])
 {
-
+    field[destrow][destcolumn][0] = field[startrow][startcolumn][0];
+    field[destrow][destcolumn][1] = field[startrow][startcolumn][1];
+    field[startrow][startcolumn][0] = 0;
 }
