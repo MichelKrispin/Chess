4b0722ddf5afc1b0e9e87e4e942df4c9fc63d87f
janlucian <jan.lucian.h3@gmail.com>
2019-01-08 16:13:31 +0100

    switch checkmove

--- CheckMove.h
+++ CheckMove.h
@@ -1,7 +1,22 @@
 // Check whether the move is valid
-int CheckMove(unsigned int startrow, unsigned int startcolumn,
-              unsigned int destrow, unsigned int destcolumn,
-              unsigned int field[][8][2])
+int CheckMove(char activePLayer, unsigned int startrow, unsigned int startcolumn, unsigned int destrow, unsigned int destcolumn, unsigned int field[][8][2])
 {
-
+    if(field[startrow][startcolumn][1] == activePLayer)
+    {
+        switch(field[startrow][startcolumn][0])
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+        }
+    }
 }
