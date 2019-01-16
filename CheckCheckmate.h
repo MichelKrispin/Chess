ae21691847c8c883fb8f2c235a1f2bd9d8cb74e1
janlucian <jan.lucian.h3@gmail.com>
2019-01-16 14:04:49 +0100

    In between commit

--- CheckCheckmate.h
+++ CheckCheckmate.h
@@ -1,16 +1,19 @@
 // Check whether any King is now in Checkmate
 int CheckCheckmate(unsigned int field[][8][2])
 {
+    // checks if there is any possible move
+    // starting
     for(unsigned int startrow = 0; startrow < 8; startrow++)
     {
         for(unsigned int startcolumn = 0; startcolumn < 8; startcolumn++)
         {
-            for(unsigned int destrow = 0; destrow < 8; destrow++)
+            for (unsigned int destrow = 0; destrow < 8; destrow++)
             {
-                for(unsigned int destcolumn = 0; destcolumn < 8; destcolumn++)
+                for (unsigned int destcolumn = 0; destcolumn < 8; destcolumn++)
                 {
-                    if(CheckMove(field[startrow][startcolumn][1], startrow, startcolumn, destrow, destcolumn, field) 
-                      && CheckLogic(field[startrow][startcolumn][1], startrow, startcolumn, destrow, destcolumn, field))
+                    if ((destrow != startrow || destcolumn != startcolumn)
+                        && CheckMove(field[startrow][startcolumn][1], startrow, startcolumn, destrow, destcolumn, field)
+                        && CheckLogic(field[startrow][startcolumn][1], startrow, startcolumn, destrow, destcolumn, field))
                         return 0;
                 }
             }
